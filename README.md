b908e325ba9f86deccef06d8eed1da2bda0504fd
Jarrett Keifer <jkeifer0@gmail.com>
2025-04-20 16:44:18 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# griffine
+
+Utilities for working with *gri*ds that have a*ffine* transforms, typically for
+working with rasters or other gridded data.
+
+## Installation
+
+This package is distributed on pypi and can be `pip`-installed:
+
+```commandline
+pip install griffine
+```
+
+## Usage
+
+This library is composed of several major classes:
+
+* `Grid`
+* `TiledGrid`
+* `AffineGrid`
+* `TiledAffineGrid`
+* `Point`
+* `Cell` and `AffineCell`
+* `Tile` and `AffineTile`
+
+`Grid` represents a two-dimensional grid of `Cell`s, with a size defined by its
+`cols` and `rows`. A `TiledGrid` is, effectively, a grid of grids. Each cell in
+a `TiledGrid` is a `Tile`. `Tiles` are both `Cell`s and `Grid`s, where the tile
+grid is a subset of the larger `Grid` that has been tiled.
+
+`AffineGrid` is a `Grid` with the addition of an affine transformation to allow
+transformations between grid/image space and model space (in the case of
+geospatial data, model space would be the raster's coordinate reference
+system). `AffineGrids` allow looking up the `Point` represented by a `Cell`
+(using its origin, centroid, or antiorigin), or the `Cell` containing a
+`Point`.
+
+A `TiledAffineGrid` is to an `AffineGrid` as a `TiledGrid` is to a `Grid`: each
+`AffileTile` in a `TiledAffineGrid` is an `AffineGrid` representing some subset
+of the larger `AffineGrid` that was tiled. `TiledAffineGrids` allow finding the
+`AffineTile` containing` a `Cell` or a `Point`.
+
+`griffine` does not handle coordinate systems and thus does not do any
+reprojection. It is expected that users ensure they are using a consistent CRS
+between the affine transforms of their grid and any points.
+
+The [Python `__geo_interface__`
+protocol](https://gist.github.com/sgillies/2217756) is supported by all
+operations accepting a `Point` and on the `Point` class itself, to easily allow
+using or casting to point geometries from other Python libraries (`shapely`,
+`odc-geo`, etc.).
+
+### Examples
+
+```python
+from affine import Affine
+from griffine import Grid, Point, Tile
+
+# 10m pixel grid in UTM coordinates
+transform = Affine(10, 0, 200000, 0, -10, 6100000)
+
+# First we create a grid!
+grid = Grid(10000, 5000)
+
+# We can tile the grid using another grid.
+# In this example we'd get a 10x5 tile grid
+# where each tile is a grid of 1000x1000.
+tile_0_0 = grid.tile(Grid(10, 5))[0][0]
+tile.size  # (1000, 1000)
+
+# We can also add an affine transform to our grid.
+# A transform allows converting between grid space and
+# model space (i.e., cell coords and spatial coords).
+affine_grid = grid.add_transform(transform)
+affine_cell_0_0 = affine_grid[0][0]
+affine_cell_0_0.origin  # Point(200000, 6100000)
+affine_cell_0_0.centroid  # Point(200005, 6099995)
+affine_cell_0_0.antiorigin  # Point(200010, 6099990)
+
+
+# Grids can be tiled by a tile size instead of a grid.
+# Here we'll get a 10x5 grid, but the left and bottom
+# edge tiles will not be full size.
+tiled_grid = grid.tile(Tile(1024, 1024))
+
+# We can add a transform to a tiled grid too.
+# Doing so gives us the ability to convert
+# between tile coordinates and model space.
+tiled_affine_grid = tiled_grid.add_transform(transform)
+point = Point(
+```
+
+## How to say "griffine"
+
+The name of this library is pronounced "grif-fine", as in the words "grift",
+and "fine".
---
+++ src/griffine/grid.py
@@ -0,0 +1,252 @@
+from __future__ import annotations
+
+import math
+
+from typing import Annotated, Generic, Protocol, Self, TypeVar, runtime_checkable
+
+from affine import Affine
+
+NonNegativeInt = Annotated[int, ">=0"]
+PositiveInt = Annotated[int, ">=1"]
+Rows = Annotated[PositiveInt, "number of rows"]
+Columns = Annotated[PositiveInt, "number of columns"]
+
+
+@runtime_checkable
+class CellType(Protocol):
+    row: NonNegativeInt
+    col: NonNegativeInt
+
+
+@runtime_checkable
+class GridType(Protocol):
+    rows: Rows
+    cols: Columns
+
+    @property
+    def size(self) -> tuple[Rows, Columns]:
+        return self.rows, self.cols
+
+    def __getitem__(
+        self,
+        row_index: NonNegativeInt,
+        col_index: NonNegativeInt,
+    ) -> GridCell[Self]:
+        if row_index >= self.rows:
+            raise IndexError('row index out of range')
+
+        if col_index >= self.cols:
+            raise IndexError('column index out of range')
+
+        return GridCell(row_index, col_index, self)
+
+
+@runtime_checkable
+class TransformableType(Protocol):
+    transform: Affine
+
+
+
+
+
+
+T = TypeVar('T', bound=GridType)
+
+
+class GridCell(Generic[T]):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        parent: T,
+    ) -> None:
+        self.row = row
+        self.col = col
+        self.parent = parent
+
+
+class GridTile(GridType, GridCell[T]):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        rows: Rows,
+        cols: Columns,
+        parent: T,
+    ) -> None:
+        self.row = row
+        self.col = col
+        self.rows = rows
+        self.cols = cols
+        self.parent = parent
+
+
+def can_tile_into(grid_size: PositiveInt, tile_count: PositiveInt) -> bool:
+    return tile_count == math.ceil(grid_size/math.ceil(grid_size/tile_count))
+
+
+class TileableType(GridType, Protocol):
+    def _tiled(
+        self,
+        grid_size: tuple[Rows, Columns],
+        tile_size: tuple[Rows, Columns],
+    ) -> Tiled[Self]:
+        raise NotImplementedError
+
+    def tile_via(self, grid: Grid) -> Tiled[Self]:
+        '''Tile self where each tile is the size of `grid`.'''
+        ...
+        if (
+            self.cols < grid.cols
+            or self.rows < grid.rows
+        ):
+            raise ValueError(
+                f'Cannot tile grid of size {self.size} with tiles of size {grid.size}',
+            )
+
+        rows = math.ceil(self.rows / grid.rows)
+        cols = math.ceil(self.cols / grid.cols)
+
+        return self._tiled(
+            (rows, cols),
+            grid.size,
+        )
+
+    def tile_into(self, grid: GridType) -> Tiled[Self]:
+        '''Tile self into the tile grid is given by `grid`.'''
+        ...
+        if not (
+            can_tile_into(self.rows, grid.rows)
+            and can_tile_into(self.cols, grid.cols)
+        ):
+            raise ValueError(
+                f'Cannot tile grid of size {self.size} into grid {grid.size}',
+            )
+
+        tile_rows = math.ceil(self.rows / grid.rows)
+        tile_cols = math.ceil(self.cols / grid.cols)
+
+        return self._tiled(
+            grid.size,
+            (tile_rows, tile_cols),
+        )
+
+
+@runtime_checkable
+class Tiled(Generic[T], Protocol):
+    base_grid: T
+    tile_rows: Rows
+    tile_cols: Columns
+
+    @property
+    def tile_size(self) -> tuple[Rows, Columns]:
+        return self.tile_rows, self.tile_cols
+
+
+class Grid(TileableType, GridType):
+    def __init__(self, rows: Rows, cols: Columns) -> None:
+        self.rows = rows
+        self.cols = cols
+
+    def _tiled(
+        self,
+        grid_size: tuple[Rows, Columns],
+        tile_size: tuple[Rows, Columns],
+    ) -> Tiled[Self]:
+        return TiledGrid(
+            rows=grid_size[0],
+            cols=grid_size[1],
+            tile_rows=tile_size[0],
+            tile_cols=tile_size[1],
+            base_grid=self,
+        )
+
+    def add_transform(self, transform: Affine) -> AffineGrid:
+        return AffineGrid(
+            self.rows,
+            self.cols,
+            transform,
+        )
+
+
+class AffineGrid(TransformableType, TileableType, GridType):
+    def __init__(self, rows: Rows, cols: Columns, transform: Affine) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.transform = transform
+
+    def _tiled(
+        self,
+        grid_size: tuple[Rows, Columns],
+        tile_size: tuple[Rows, Columns],
+    ) -> Tiled[Self]:
+        return TiledAffineGrid(
+            rows=grid_size[0],
+            cols=grid_size[1],
+            tile_rows=tile_size[0],
+            tile_cols=tile_size[1],
+            base_grid=self,
+            transform=Affine(
+                self.transform.a * tile_size[0],
+                self.transform.b,
+                self.transform.c,
+                self.transform.d,
+                self.transform.e * tile_size[1],
+                self.transform.f,
+            ),
+        )
+
+
+class TiledGrid(Tiled, GridType):
+    def __init__(
+        self,
+        rows: Rows,
+        cols: Columns,
+        tile_rows: Rows,
+        tile_cols: Columns,
+        base_grid: Grid,
+    ) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.tile_rows = tile_rows
+        self.tile_cols = tile_cols
+        self.base_grid = base_grid
+
+    def add_transform(self, transform: Affine) -> TiledAffineGrid:
+        base = self.base_grid.add_transform(
+            Affine(
+                transform.a / self.tile_cols,
+                transform.b,
+                transform.c,
+                transform.d,
+                transform.e / self.tile_rows,
+                transform.f,
+            ),
+        )
+        return TiledAffineGrid(
+            self.rows,
+            self.cols,
+            self.tile_rows,
+            self.tile_cols,
+            base,
+            transform,
+        )
+
+
+class TiledAffineGrid(Tiled, TransformableType, GridType):
+    def __init__(
+        self,
+        rows: Rows,
+        cols: Columns,
+        tile_rows: Rows,
+        tile_cols: Columns,
+        base_grid: AffineGrid,
+        transform: Affine,
+    ) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.tile_rows = tile_rows
+        self.tile_cols = tile_cols
+        self.base_grid = base_grid
+        self.transform = transform
+    # TODO: need to make sure base grid gets appropriate transform?
