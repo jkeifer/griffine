2fdd2fc95103be25b518747969a2aa42b423823c
Jarrett Keifer <jkeifer0@gmail.com>
2025-04-22 21:58:12 -0700

    wip

--- src/griffine/grid.py
+++ src/griffine/grid.py
@@ -1,34 +1,34 @@
 from __future__ import annotations
 
-import math
-
-from typing import Annotated, Self
-
 from affine import Affine
 
 from griffine.types import (
+    AffineGridTileType,
+    CellType,
+    Columns,
+    GridTileType,
     GridType,
+    NonNegativeInt,
+    Rows,
     TileableType,
-    TiledType,
+    TiledCellType,
+    TiledGridType,
+    TileType,
+    TransformableGridType,
+    TransformableTiledGridType,
     TransformableType,
 )
 
-NonNegativeInt = Annotated[int, ">=0"]
-PositiveInt = Annotated[int, ">=1"]
-Rows = Annotated[PositiveInt, "number of rows"]
-Columns = Annotated[PositiveInt, "number of columns"]
 
+class Grid(TileableType['GridCell', 'TiledGrid']):
+    def _get_cell(self, row: NonNegativeInt, col: NonNegativeInt) -> GridCell:
+        return GridCell(row=row, col=col, parent_grid=self)
 
-def can_tile_into(grid_size: PositiveInt, tile_count: PositiveInt) -> bool:
-    return tile_count == math.ceil(grid_size/math.ceil(grid_size/tile_count))
-
-
-class Grid(TileableType, GridType):
     def _tiled(
         self,
         grid_size: tuple[Rows, Columns],
         tile_size: tuple[Rows, Columns],
-    ) -> TiledType[Self]:
+    ) -> TiledGrid:
         return TiledGrid(
             rows=grid_size[0],
             cols=grid_size[1],
@@ -45,16 +45,21 @@ class Grid(TileableType, GridType):
         )
 
 
-class AffineGrid(TransformableType, TileableType, GridType):
+class AffineGrid(
+    TileableType['AffineGridCell', 'TiledAffineGrid'],
+    TransformableGridType,
+):
     def __init__(self, rows: Rows, cols: Columns, transform: Affine) -> None:
         super().__init__(rows=rows, cols=cols, transform=transform)
+
+    def _get_cell(self, row: NonNegativeInt, col: NonNegativeInt) -> AffineGridCell:
+        return AffineGridCell(row=row, col=col, parent_grid=self)
 
     def _tiled(
         self,
         grid_size: tuple[Rows, Columns],
         tile_size: tuple[Rows, Columns],
-    ) -> TiledType[Self]:
-        # TODO: move this logic to a constructor on TiledAffineGrid
+    ) -> TiledAffineGrid:
         return TiledAffineGrid(
             rows=grid_size[0],
             cols=grid_size[1],
@@ -72,7 +77,7 @@ class AffineGrid(TransformableType, TileableType, GridType):
         )
 
 
-class TiledGrid(TiledType, GridType):
+class TiledGrid(TiledGridType[Grid, 'GridTile']):
     def __init__(
         self,
         rows: Rows,
@@ -89,8 +94,10 @@ class TiledGrid(TiledType, GridType):
             base_grid=base_grid,
         )
 
+    def _get_cell(self, row: NonNegativeInt, col: NonNegativeInt) -> GridTile:
+        return GridTile(row=row, col=col, parent_grid=self)
+
     def add_transform(self, transform: Affine) -> TiledAffineGrid:
-        # TODO: move this logic to a constructor on TiledAffineGrid
         base = self.base_grid.add_transform(
             Affine(
                 transform.a / self.tile_cols,
@@ -111,7 +118,9 @@ class TiledGrid(TiledType, GridType):
         )
 
 
-class TiledAffineGrid(TiledType, TransformableType, GridType):
+class TiledAffineGrid(
+    TransformableTiledGridType[AffineGrid, 'AffineGridTile'],
+):
     def __init__(
         self,
         rows: Rows,
@@ -128,4 +137,148 @@ class TiledAffineGrid(TiledType, TransformableType, GridType):
             tile_cols=tile_cols,
             base_grid=base_grid,
             transform=transform,
+        )
+
+    def _get_cell(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+    ) -> AffineGridTile:
+        return AffineGridTile(row=row, col=col, parent_grid=self)
+
+
+class Cell(CellType):
+    def __init__(self, row: NonNegativeInt, col: NonNegativeInt) -> None:
+        super().__init__(row=row, col=col)
+
+
+class GridCell(CellType):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        parent_grid: GridType,
+    ) -> None:
+        super().__init__(row=row, col=col)
+        self.parent_grid = parent_grid
+
+
+class TiledGridCell(TiledCellType):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        tile_row: NonNegativeInt,
+        tile_col: NonNegativeInt,
+        parent_grid: GridTileType,
+    ) -> None:
+        super().__init__(
+            row=row,
+            col=col,
+            tile_row=tile_row,
+            tile_col=tile_col,
+            parent_grid=parent_grid,
+        )
+
+
+class AffineGridCell(CellType, TransformableType):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        parent_grid: TransformableGridType,
+    ) -> None:
+        # TODO: transform for cell from parent_grid
+        transform = parent_grid.transform
+        super().__init__(row=row, col=col, transform=transform)
+        self.parent_grid = parent_grid
+
+
+class TiledAffineGridCell(
+    TiledCellType,
+    TransformableType,
+):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        tile_row: NonNegativeInt,
+        tile_col: NonNegativeInt,
+        parent_grid: AffineGridTileType,
+    ) -> None:
+        # TODO: transform for cell from parent_grid
+        transform = parent_grid.transform
+        super().__init__(
+            row=row,
+            col=col,
+            tile_row=tile_row,
+            tile_col=tile_col,
+            parent_grid=parent_grid,
+            transform=transform,
+        )
+
+
+class Tile(TileType[GridCell], CellType):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        rows: Rows,
+        cols: Columns,
+    ) -> None:
+        super().__init__(row=row, col=col, rows=rows, cols=cols)
+
+    def _get_cell(self, row: NonNegativeInt, col: NonNegativeInt) -> GridCell:
+        return GridCell(row=row, col=col, parent_grid=self)
+
+
+class GridTile(GridTileType[TiledGridCell], CellType):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        parent_grid: TiledGridType,
+    ) -> None:
+        super().__init__(
+            row=row,
+            col=col,
+            parent_grid=parent_grid,
+        )
+
+    def _get_cell(self, row: NonNegativeInt, col: NonNegativeInt) -> TiledGridCell:
+        row, col = self.tile_coords_to_parent_coords(row, col)
+        return TiledGridCell(
+            row=row,
+            col=col,
+            tile_row=self.row,
+            tile_col=self.col,
+            parent_grid=self,
+        )
+
+
+class AffineGridTile(AffineGridTileType[TiledAffineGridCell]):
+    def __init__(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+        parent_grid: TransformableTiledGridType,
+    ) -> None:
+        super().__init__(
+            row=row,
+            col=col,
+            parent_grid=parent_grid,
+        )
+
+    def _get_cell(
+        self,
+        row: NonNegativeInt,
+        col: NonNegativeInt,
+    ) -> TiledAffineGridCell:
+        row, col = self.tile_coords_to_parent_coords(row, col)
+        return TiledAffineGridCell(
+            row=row,
+            col=col,
+            tile_row=self.row,
+            tile_col=self.col,
+            parent_grid=self,
         )
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,3 +1,4 @@
+from pygeoif import Point
 import pytest
 
 from griffine import (
@@ -5,20 +6,33 @@ from griffine import (
     Grid,
     OutOfBoundsError,
 )
+from griffine.exceptions import InvalidCoordinateError, InvalidGridError, InvalidTilingError
+from griffine.grid import Cell
 
 
 @pytest.mark.parametrize(
-    'rows,cols',
+    ('rows' , 'cols'),
     [
         (10000, 5000),
         (1, 1),
-        #(0, 0),
-        #(-1, -1),
-        #(0, 1000),
     ],
 )
 def test_grid_constructor(rows: int, cols: int) -> None:
     _ = Grid(rows, cols)
+
+
+@pytest.mark.parametrize(
+    ('rows' , 'cols'),
+    [
+        (0, 0),
+        (-1, -1),
+        (0, 1000),
+        (100, -1343),
+    ],
+)
+def test_grid_constructor_bad(rows: int, cols: int) -> None:
+    with pytest.raises(InvalidGridError):
+        Grid(rows, cols)
 
 
 def test_grid_size() -> None:
@@ -26,15 +40,41 @@ def test_grid_size() -> None:
 
 
 @pytest.mark.parametrize(
+    ('row' , 'col'),
+    [
+        (0, 0),
+        (0, 1000),
+        (1000, 0),
+        (210134303, 20120404034),
+    ],
+)
+def test_cell_constructor(row: int, col: int) -> None:
+    _ = Cell(row, col)
+
+
+@pytest.mark.parametrize(
+    ('row' , 'col'),
+    [
+        (-1, 0),
+        (0, -1000),
+        (-1000, -24),
+    ],
+)
+def test_cell_constructor_bad(row: int, col: int) -> None:
+    with pytest.raises(InvalidCoordinateError):
+        Cell(row, col)
+
+
+@pytest.mark.parametrize(
     'coords',
     [
         (5032, 42),
         (42, 4099),
-        #(0, 101),
-        #(101, 0),
-        #(0, 0),
+        (0, 101),
+        (101, 0),
+        (0, 0),
         (-1, 1000),
-        #(0, -1000),
+        (0, -1000),
     ],
 )
 def test_grid_get_cell(coords: tuple[int, int]) -> None:
@@ -43,7 +83,7 @@ def test_grid_get_cell(coords: tuple[int, int]) -> None:
     cell = grid[coords]
     assert cell.row == coords[0] or cell.row == coords[0] + rows
     assert cell.col == coords[1] or cell.col == coords[1] + cols
-    assert cell.grid is grid
+    assert cell.parent_grid is grid
 
 
 @pytest.mark.parametrize(
@@ -52,7 +92,7 @@ def test_grid_get_cell(coords: tuple[int, int]) -> None:
         (10000, 42),
         (42, 5000),
         (-10001, 1000),
-        #(104300403, 0),
+        (104300403, 0),
     ],
 )
 def test_grid_get_cell_bad_index(coords: tuple[int, int]) -> None:
@@ -68,7 +108,7 @@ def test_grid_add_transform() -> None:
 
 
 @pytest.mark.parametrize(
-    'coords,index',
+    ('coords', 'index'),
     [
         ((0, 0), 0),
         ((9, 9), 99),
@@ -84,7 +124,7 @@ def test_grid_cell_linear_index(
     rows, cols = 10, 10
     grid = Grid(rows, cols)
     cell = grid[coords]
-    linear_index = cell.linear_index
+    linear_index = grid.linear_index(cell)
     assert linear_index == index
 
 
@@ -100,15 +140,94 @@ def test_grid_tile_via() -> None:
     assert tile1.col == 0
     assert tile1.rows == 1024
     assert tile1.cols == 1024
+    assert tile1.size == (1024, 1024)
     cell1 = tile1[0, 0]
     assert cell1.row == 0
     assert cell1.col == 0
+    assert cell1.tile_row == 0
+    assert cell1.tile_col == 0
     tile2 = tiled[9, 4]
     assert tile2.row == 9
     assert tile2.col == 4
-    assert tile2.rows == 1000
-    assert tile2.cols == 1000
+    assert tile2.rows == 784
+    assert tile2.cols == 904
+    assert tile2.size == (784, 904)
     cell2 = tile2[0, 0]
-    assert cell2.row == 0
-    assert cell2.col == 0
+    assert cell2.row == 9216
+    assert cell2.col == 4096
+    assert cell1.tile_row == 0
+    assert cell1.tile_col == 0
 
+
+def test_grid_tile_into() -> None:
+    grid = Grid(10000, 5000)
+    tiled = grid.tile_into(Grid(10, 6))
+    assert tiled.size == (10, 6)
+    assert tiled.base_grid is grid
+    assert tiled.tile_rows == 1000
+    assert tiled.tile_cols == 834
+    assert tiled.tile_size == (1000, 834)
+    tile1 = tiled[0, 0]
+    assert tile1.row == 0
+    assert tile1.col == 0
+    assert tile1.rows == 1000
+    assert tile1.cols == 834
+    assert tile1.size == (1000, 834)
+    cell1 = tile1[0, 0]
+    assert cell1.row == 0
+    assert cell1.col == 0
+    assert cell1.tile_row == 0
+    assert cell1.tile_col == 0
+    assert cell1.size == (1, 1)
+    tile2 = tiled[9, 5]
+    assert tile2.row == 9
+    assert tile2.col == 5
+    assert tile2.rows == 1000
+    assert tile2.cols == 830
+    cell2 = tile2[0, 0]
+    assert cell2.row == 9000
+    assert cell2.col == 4170
+    assert cell1.tile_row == 0
+    assert cell1.tile_col == 0
+
+
+@pytest.mark.parametrize(
+    'coords',
+    [
+        (4, 4),
+        (0, 9),
+        (12, 0),
+    ],
+)
+def test_grid_tile_bad_coords(coords: tuple[int, int]) -> None:
+    with pytest.raises(OutOfBoundsError):
+        Grid(1,1).tile_into(Grid(1,1))[coords]
+
+
+
+def test_affine_grid_ops() -> None:
+    affine_grid = Grid(10, 5).add_transform(Affine(10, 0, 200000, 0, -10, 6100000))
+    assert affine_grid.width == 5 * 10
+    assert affine_grid.heigth == 10 * -10
+    assert affine_grid.origin == Point(200000, 6100000)
+    assert affine_grid.centroid == Point(
+        200000 + (affine_grid.width / 2),
+        6100000 + (affine_grid.heigth / 2),
+    )
+    assert affine_grid.antiorigin == Point(
+        200000 + affine_grid.width,
+        6100000 + affine_grid.heigth,
+    )
+
+
+@pytest.mark.parametrize(
+    ('grid', 'tile_grid'),
+    [
+        ((4, 4), (3, 3)),
+        ((9, 9), (8, 1)),
+        ((12, 12), (12, 11)),
+    ],
+)
+def test_tiling_error1(grid: tuple[int, int], tile_grid: tuple[int, int]) -> None:
+    with pytest.raises(InvalidTilingError):
+        Grid(*grid).tile_into(Grid(*tile_grid))
